3c1cc6ea630458629324dfb73b31135494971d10
DevFaulk <faulkdev@gmail.com>
2023-12-04 15:59:23 -0500

    add site comment

--- exercises/gif-test/index.js
+++ exercises/gif-test/index.js
@@ -1,5 +1,6 @@
 // let categorySelect = document.getElementById('categorySelect');
 // let loadGifs = document.getElementById('loadGifs');
+// site: https://developers.giphy.com/dashboard/
 
 fetch(
   'https://api.giphy.com/v1/gifs/categories?api_key=8eHJcp4BuWNQ25hMAXNYT8yIzSDGJwvC'
