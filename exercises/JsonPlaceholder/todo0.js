02ce25220570f759550472fee2e7f73a07351b9b
Teddy Faulk <FaulkDev@gmail.com>
2023-12-05 11:41:44 -0500

    solve exercise

--- exercises/JsonPlaceholder/todo0.js
+++ exercises/JsonPlaceholder/todo0.js
@@ -6,11 +6,8 @@ const submitTodo = document.getElementById('submitTodo');
 const todoData = document.getElementById('todoData');
 
 async function fetchTodo() {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoInput.value}`
-  );
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
   const data = await response.json();
-  
+
   todoData.innerText = data.title;
 }
-
